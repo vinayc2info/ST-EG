2e55a49012a83a5afd15eb6137eb1a63e5122d79
Vinay Kumar <vinay.ks@c2info.com>
2022-07-15 16:12:05 +0530

    procedure corrections done

--- BlackTrayScripts/Procedures.sql
+++ BlackTrayScripts/Procedures.sql
@@ -35,19 +35,11 @@ declare @carton_no char(6);
   when 'supervisor_dashboard' then
     select 
         c_err_mark_user as conversion_user,
-        c_item_code as item_code,
+        st_err_track_det.c_item_code as item_code,
         item_mst.c_name as item_name,
-        c_batch_no as batch_no,
-        n_qty as err_qty,
-        (select sum(stock.n_bal_qty - stock.n_hold_qty - isnull(stk_godown.godown_bal_qty,0)) 
-        from stock 
-        left outer join (select c_br_code, c_item_code, c_batch_no, sum(stock_godown.n_qty - stock_godown.n_hold_qty) as godown_bal_qty from stock_godown 
-                    group by  c_br_code, c_item_code, c_batch_no) stk_godown on 
-                stock.c_item_code = stk_godown.c_item_code 
-                and stock.c_batch_no = stk_godown.c_batch_no
-                and stock.c_br_code = stk_godown.c_br_code 
-        where stock.c_br_code = isnull(stk_godown.c_br_code,uf_get_br_code('000')) and stock.c_item_code = st_err_track_det.c_item_code and stock.c_batch_no = st_err_track_det.c_batch_no
-        ) as stk_bal_qty,
+        st_err_track_det.c_batch_no as batch_no,
+        st_err_track_det.n_qty as err_qty,
+        isnull(stock.n_bal_qty, 0) - isnull(stock.n_hold_qty, 0) - isnull(stk_godown.godown_bal_qty,0) as stk_bal_qty,
         n_err_type as err_type,
         CASE err_type
             WHEN 0 THEN 'ITEM SHORT'
@@ -61,8 +53,16 @@ declare @carton_no char(6);
         t_err_time as err_req_time,
         c_supervisor as spvr_name,
         t_spvr_err_marked_time as spvr_err_marked_time
-    from st_err_track_det
+    from st_err_track_dets
     join item_mst on item_mst.c_code = st_err_track_det.c_item_code
+    left join stock on stock.c_br_code = uf_get_br_code('000') and stock.c_item_code = st_err_track_det.c_item_code and stock.c_batch_no = st_err_track_det.c_batch_no
+    left join (select c_br_code, c_item_code, c_batch_no, sum(stock_godown.n_qty - stock_godown.n_hold_qty) as godown_bal_qty 
+                  from stock_godown 
+                  group by  c_br_code, c_item_code, c_batch_no
+              ) stk_godown 
+              on stock.c_item_code = stk_godown.c_item_code 
+              and stock.c_batch_no = stk_godown.c_batch_no
+              and stock.c_br_code = stk_godown.c_br_code
     where st_err_track_det.n_complete = 0 for xml raw,elements
   end case
 end;
