66d57523efaf1b374978c763d32100e03036072c
Vinay Kumar <vinay.ks@c2info.com>
2022-07-15 13:20:25 +0530

    added stk_bal_qty

--- BlackTrayScripts/Procedures.sql
+++ BlackTrayScripts/Procedures.sql
@@ -1,3 +1,10 @@
+if (select count() from syswebservice where service_name = 'ws_st_err_tracking') = 0 then
+    CREATE SERVICE "ws_st_err_tracking" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ELEMENTS AS select '<root>'+"is_xml_string"+'</root>'
+      from "DBA"."usp_st_err_tracking"(:url1,:url2,:url3,:url4,:url5,:url6,:url7,:url8,:url9,:url10);
+end if ;
+commit work;
+go
+
 create or replace procedure usp_st_err_tracking(  
   in @gsBr char(6),
   in @devID char(200),
@@ -32,7 +39,15 @@ declare @carton_no char(6);
         item_mst.c_name as item_name,
         c_batch_no as batch_no,
         n_qty as err_qty,
-        45 as stk_bal_qty,
+        (select sum(stock.n_bal_qty - stock.n_hold_qty - isnull(stk_godown.godown_bal_qty,0)) 
+        from stock 
+        left outer join (select c_br_code, c_item_code, c_batch_no, sum(stock_godown.n_qty - stock_godown.n_hold_qty) as godown_bal_qty from stock_godown 
+                    group by  c_br_code, c_item_code, c_batch_no) stk_godown on 
+                stock.c_item_code = stk_godown.c_item_code 
+                and stock.c_batch_no = stk_godown.c_batch_no
+                and stock.c_br_code = stk_godown.c_br_code 
+        where stock.c_br_code = isnull(stk_godown.c_br_code,uf_get_br_code('000')) and stock.c_item_code = st_err_track_det.c_item_code and stock.c_batch_no = st_err_track_det.c_batch_no
+        ) as stk_bal_qty,
         n_err_type as err_type,
         CASE err_type
             WHEN 0 THEN 'ITEM SHORT'
@@ -54,10 +69,3 @@ end;
 commit work;
 go
 
-
-if (select count() from syswebservice where service_name = 'ws_st_err_tracking') = 0 then
-    CREATE SERVICE "ws_st_err_tracking" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ELEMENTS AS select '<root>'+"is_xml_string"+'</root>'
-      from "DBA"."usp_st_err_tracking"(:url1,:url2,:url3,:url4,:url5,:url6,:url7,:url8,:url9,:url10);
-end if ;
-commit work;
-go
