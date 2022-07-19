8aca9fde27c2cd89aa1fb8f9d6e3b79c0c5efe0d
Vinay Kumar <vinay.ks@c2info.com>
2022-07-19 18:26:31 +0530

    function added to get the new_tran_no

--- BlackTrayScripts/Procedures.sql
+++ BlackTrayScripts/Procedures.sql
@@ -1,3 +1,48 @@
+CREATE OR REPLACE FUNCTION uf_get_new_tran( in as_tran_code char(5), in as_prefix char(6)) 
+returns numeric(18)
+begin
+  declare d_sr_no numeric(18);
+  declare d_sr_no_b4 numeric(18);
+  declare as_br char(3);
+  declare as_year char(2);
+  declare @sql_code numeric(20);
+  set as_br = uf_get_br_code('000');
+  set as_year = RIGHT(db_name(),2);
+  select prefix_serial_no.n_sr_number
+    into d_sr_no_b4 from prefix_serial_no
+    where prefix_serial_no.c_br_code = as_br
+    and prefix_serial_no.c_year = as_year
+    and prefix_serial_no.c_prefix = as_prefix
+    and prefix_serial_no.c_trans = as_tran_code;
+  //generate new transaction number
+  update prefix_serial_no
+    set n_sr_number = n_sr_number+1
+    where  prefix_serial_no.c_br_code = as_br
+    and prefix_serial_no.c_year = as_year
+    and prefix_serial_no.c_prefix = as_prefix
+    and prefix_serial_no.c_trans = as_tran_code;
+  select prefix_serial_no.n_sr_number
+    into d_sr_no from prefix_serial_no
+    where  prefix_serial_no.c_br_code = as_br
+    and prefix_serial_no.c_year = as_year
+    and prefix_serial_no.c_prefix = as_prefix
+    and prefix_serial_no.c_trans = as_tran_code;
+  if d_sr_no = d_sr_no_b4 then
+    set d_sr_no = 0
+  end if;
+  return d_sr_no
+exception
+  when others then
+    set @sql_code = sqlcode;
+    if errormsg(@sql_code) is not null then
+      set d_sr_no = 999999999
+    end if;
+    return d_sr_no
+end;
+COMMIT WORK;
+GO 
+
+
 create or replace procedure usp_st_err_tracking(  
   in @gsBr char(6),
   in @devID char(200),
@@ -32,7 +77,7 @@ begin
   end if;
   set @BrCode = uf_get_br_code(@gsBr);
   set @year = right(db_name(),2);
-  set @prefix ='500';
+  set @prefix ='Z';
   set @trans ='BTD';
   case @cIndex
   when 'supervisor_dashboard' then
@@ -81,16 +126,13 @@ begin
   when 'supervisor_approve' then
     CASE err_type
         WHEN 0 THEN --'ITEM SHORT'
-            update prefix_serial_no set n_sr_number = n_sr_number + 1  where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
-            select n_sr_number into @tran_srno from prefix_serial_no where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
+            select uf_get_new_tran(@trans,@prefix) into @tran_srno;
         WHEN 1 THEN --'ITEM EXCESS'
             print 'ITEM EXCESS';
         WHEN 2 THEN --'ITEM BREAKAGE/DAMAGED'
-            update prefix_serial_no set n_sr_number = n_sr_number + 1  where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
-            select n_sr_number into @tran_srno from prefix_serial_no where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
+            select uf_get_new_tran(@trans,@prefix) into @tran_srno;            
         WHEN 3 THEN --'WRONG ITEM'
-            update prefix_serial_no set n_sr_number = n_sr_number + 1  where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
-            select n_sr_number into @tran_srno from prefix_serial_no where c_br_code = @BrCode and c_year = @year and c_prefix =@prefix and c_trans = @trans;
+            select uf_get_new_tran(@trans,@prefix) into @tran_srno;
         WHEN 4 THEN --'BATCH MISMATCH'
             print 'BATCH MISMATCH'
         ELSE --'NO REASON'
