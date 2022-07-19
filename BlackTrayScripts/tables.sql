c4e56be52a596119073925ee89e99680b38c6d9d
Vinay Kumar <vinay.ks@c2info.com>
2022-07-19 15:58:26 +0530

    black_tray_mst_det tables added

--- BlackTrayScripts/tables.sql
+++ BlackTrayScripts/tables.sql
@@ -44,18 +44,18 @@ go
 
 if (select count() from systable where creator = 1 and table_name = 'black_tray_mst') = 0 then 
   CREATE TABLE black_tray_mst (
-  	c_br_code CHAR(6) NOT NULL,
-  	c_year CHAR(2) NOT NULL,
-  	c_prefix CHAR(4) NOT NULL,
-  	n_srno NUMERIC(9,0) NOT NULL,
+    c_br_code CHAR(6) NOT NULL,
+    c_year CHAR(2) NOT NULL,
+    c_prefix CHAR(4) NOT NULL,
+    n_srno NUMERIC(9,0) NOT NULL,
     n_inout numeric(1,0) not null,
-  	d_date DATE NOT NULL,
+    d_date DATE NOT NULL,
     c_cust_code CHAR(6) NOT NULL,
     c_supervisor CHAR(15) NULL,
-  	n_store_track NUMERIC(1,0) NULL DEFAULT 0,
-  	d_ldate DATE NULL,
-  	t_ltime TIMESTAMP NULL DEFAULT CURRENT TIMESTAMP,
-  	PRIMARY KEY ( c_br_code ASC, c_year ASC, c_prefix ASC, n_srno ASC, n_inout ASC )
+    n_store_track NUMERIC(1,0) NULL DEFAULT 0,
+    d_ldate DATE NULL,
+    t_ltime TIMESTAMP NULL DEFAULT CURRENT TIMESTAMP,
+  PRIMARY KEY ( c_br_code ASC, c_year ASC, c_prefix ASC, n_srno ASC, n_inout ASC )
   ) IN system;
 end if;
 commit work;
@@ -63,26 +63,26 @@ go
 
 if (select count() from systable where creator = 1 and table_name = 'black_tray_det') = 0 then 
   CREATE TABLE black_tray_det (
-  	c_br_code CHAR(6) NOT NULL,
-  	c_year CHAR(2) NOT NULL,
-  	c_prefix CHAR(4) NOT NULL,
-  	n_srno NUMERIC(9,0) NOT NULL,
+    c_br_code CHAR(6) NOT NULL,
+    c_year CHAR(2) NOT NULL,
+    c_prefix CHAR(4) NOT NULL,
+    n_srno NUMERIC(9,0) NOT NULL,
     n_inout numeric(1,0) not null,
-  	n_seq NUMERIC(4,0) NOT NULL,
-  	d_date DATE NULL,
-  	c_item_code CHAR(6) NOT NULL,
-  	c_batch_no CHAR(15) NOT NULL,
-  	n_qty NUMERIC(11,3) NOT NULL DEFAULT 0,
-  	c_rack CHAR(6) NULL,
+    n_seq NUMERIC(4,0) NOT NULL,
+    d_date DATE NULL,
+    c_item_code CHAR(6) NOT NULL,
+    c_batch_no CHAR(15) NOT NULL,
+    n_qty NUMERIC(11,3) NOT NULL DEFAULT 0,
+    c_rack CHAR(6) NULL,
     c_rack_grp_code CHAR(6) NULL,
     c_stage_code CHAR(6) NULL,
     c_godown_code CHAR(6) NULL,
-  	c_black_tray_code CHAR(6) NOT NULL,	
+    c_black_tray_code CHAR(6) NOT NULL,	
     n_err_type NUMERIC(1,0) NOT NULL,
-  	n_store_track NUMERIC(1,0) NULL DEFAULT 0,
-  	d_ldate DATE NULL,
-  	t_ltime TIMESTAMP NULL DEFAULT CURRENT TIMESTAMP,
-  	PRIMARY KEY ( c_br_code ASC, c_year ASC, c_prefix ASC, n_srno ASC, n_inout ASC, n_seq ASC )
+    n_store_track NUMERIC(1,0) NULL DEFAULT 0,
+    d_ldate DATE NULL,
+    t_ltime TIMESTAMP NULL DEFAULT CURRENT TIMESTAMP,
+  PRIMARY KEY ( c_br_code ASC, c_year ASC, c_prefix ASC, n_srno ASC, n_inout ASC, n_seq ASC )
   ) IN system;
 end if;
 commit work;
