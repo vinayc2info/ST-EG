1ea3503db1e216e47161c644604d1099117f4895
Vinay Kumar <vinay.ks@c2info.com>
2022-07-20 12:04:05 +0530

    tested script usp_st_dispatch_area_dash_board

--- Procedures/usp_st_dispatch_area_dash_board.sql
+++ Procedures/usp_st_dispatch_area_dash_board.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE "DBA"."usp_st_dispatch_area_dash_board"( 
+CREATE or REPLACE PROCEDURE "DBA"."usp_st_dispatch_area_dash_board"( 
   in @gsBr char(6), --1
   in @devID char(200), --2
   in @sKey char(20), --3
@@ -34,25 +34,25 @@ http://192.168.1.69:15503/ws_st_dispatch_area_dash_board?devID=devID&sKey=KEY&Us
   declare @RowPos integer;
   declare @ColMaxLen numeric(4);
   declare @RowMaxLen numeric(4);
-  //  declare local temporary table "uas_list"(
-  //    "c_stage_code" char(6) null,
-  //    "c_doc_no" char(50) null,
-  //    "c_cust_code" char(6) null,
-  //    "c_cust_name" char(100) null,
-  //    "n_completed" numeric(6) null,
-  //    "c_tray" char(50) null,
-  //    "c_route" char(50) null,
-  //    "c_route_code" char(6) null,) on commit preserve rows;
-  //  declare local temporary table "packing_done"(
-  //    "c_tray" char(6) null,
-  //    "c_doc_no" char(50) null,
-  //    "c_cust_code" char(6) null,
-  //    "c_cust_name" char(100) null,
-  //    "n_completed" numeric(6) null,
-  //    "c_route" char(50) null,
-  //    "c_route_code" char(6) null,
-  //    "n_tray_state" numeric(6) null,
-  //    ) on commit preserve rows;
+  declare local temporary table "uas_list"(
+    "c_stage_code" char(6) null,
+    "c_doc_no" char(50) null,
+    "c_cust_code" char(6) null,
+    "c_cust_name" char(100) null,
+    "n_completed" numeric(6) null,
+    "c_tray" char(50) null,
+    "c_route" char(50) null,
+    "c_route_code" char(6) null,) on commit preserve rows;
+  declare local temporary table "packing_done"(
+    "c_tray" char(6) null,
+    "c_doc_no" char(50) null,
+    "c_cust_code" char(6) null,
+    "c_cust_name" char(100) null,
+    "n_completed" numeric(6) null,
+    "c_route" char(50) null,
+    "c_route_code" char(6) null,
+    "n_tray_state" numeric(6) null,
+    ) on commit preserve rows;
   if @devID = '' or @devID is null then
     set @gsBr = "http_variable"('gsBr'); --1
     set @devID = "http_variable"('devID'); --2
@@ -62,11 +62,6 @@ http://192.168.1.69:15503/ws_st_dispatch_area_dash_board?devID=devID&sKey=KEY&Us
     set @GodownCode = "http_variable"('GodownCode'); --6
     set @HdrData = "http_variable"('HdrData') --7
   end if;
-  /*insert into "API_LOG"
-( "c_api_name","c_index","t_start_time","c_remark","c_note","c_user","n_n1" ) values
-( 'usp_st_dispatch_area_dash_board',@cIndex,"GETDATE"(),@HdrData,@devID,@UserId,"connection_property"('NUMBER') ) ;
-commit work;
-*/
   select "c_delim" into @ColSep from "ps_tab_delimiter" where "n_seq" = 0;
   select "c_delim" into @RowSep from "ps_tab_delimiter" where "n_seq" = 1;
   set @ColMaxLen = "Length"(@ColSep);
@@ -75,84 +70,47 @@ commit work;
   set @BrCode = "uf_get_br_code"(@gsBr);
   set @t_ltime = "left"("now"(),19);
   set @d_ldate = "uf_default_date"();
-  //      if(select "count"() from "block_api") >= 1 then
-  //        return
-  //      end if;
   --select 'sani ' as a for xml raw,elements  ;
   case @cIndex
   when 'get_data' then
     --insert into Serv_log  (c_name,t_ltime) values('start ',now());
-    //        insert into "uas_list"
-    //          select distinct "st_track_det"."c_stage_code" as "c_stage_code",
-    //            "st_track_mst"."c_doc_no" as "c_doc_no",
-    //            "cust"."c_code" as "c_cust_code",
-    //            "cust"."c_name" as "c_cust_name",
-    //            0 as "n_completed",
-    //            'UA-'+"isnull"("c_stage_code",'NULL') as "c_tray",
-    //            ("route_mst"."c_name") as "c_route",
-    //            "route_mst"."c_code" as "c_route_code"
-    //            from "st_track_mst"
-    //              join "DBA"."st_track_det" on "st_track_det"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_det"."n_inout" = "st_track_mst"."n_inout"
-    //              join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
-    //              left outer join "st_track_tray_move" as "tray_move" on "tray_move"."c_doc_no" = "st_track_mst"."c_doc_no"
-    //              left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
-    //              left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
-    //            where "st_track_det"."c_tray_code" is null
-    //            and "isnull"("st_track_det"."c_godown_code",'-') = '-'
-    //            and "st_track_mst"."n_confirm" = 1
-    //            and "st_track_mst"."n_inout" not in( 1,8 ) ;
-    //        insert into "packing_done"
-    //          select distinct "st_track_pick"."c_tray_code" as "c_tray",
-    //            "st_track_pick"."c_doc_no" as "c_doc_no",
-    //            "cust"."c_code" as "c_cust_code",
-    //            "cust"."c_name" as "c_cust_name",
-    //            2 as "n_completed",
-    //            "isnull"("route_mst"."c_name",'') as "c_route",
-    //            "route_mst"."c_code" as "c_route_code",
-    //            0 as "n_tray_state"
-    //            from "DBA"."st_track_pick" left outer join "st_track_complete_date" on "st_track_pick"."c_doc_no" = "st_track_complete_date"."c_doc_no"
-    //              and "st_track_pick"."c_tray_code" = "st_track_complete_date"."c_tray_code"
-    //              join "st_track_mst" on "st_track_pick"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_pick"."n_inout" = "st_track_mst"."n_inout"
-    //              join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
-    //              left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
-    //              left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
-    //            where "date"("isnull"("st_track_complete_date"."t_time","st_track_pick"."t_time")) = "DBA"."uf_default_date"()
-    //            and "isnull"("n_confirm_qty",0)+"isnull"("n_reject_qty",0) > 0;
+    insert into "uas_list"
+      select distinct "st_track_det"."c_stage_code" as "c_stage_code",
+        "st_track_mst"."c_doc_no" as "c_doc_no",
+        "cust"."c_code" as "c_cust_code",
+        "cust"."c_name" as "c_cust_name",
+        0 as "n_completed",
+        'UA-'+"isnull"("c_stage_code",'NULL') as "c_tray",
+        ("route_mst"."c_name") as "c_route",
+        "route_mst"."c_code" as "c_route_code"
+        from "st_track_mst"
+          join "DBA"."st_track_det" on "st_track_det"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_det"."n_inout" = "st_track_mst"."n_inout"
+          join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
+          left outer join "st_track_tray_move" as "tray_move" on "tray_move"."c_doc_no" = "st_track_mst"."c_doc_no"
+          left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
+          left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
+        where "st_track_det"."c_tray_code" is null
+        and "isnull"("st_track_det"."c_godown_code",'-') = '-'
+        and "st_track_mst"."n_confirm" = 1
+        and "st_track_mst"."n_inout" not in( 1,8 ) ;
+    insert into "packing_done"
+      select distinct "st_track_pick"."c_tray_code" as "c_tray",
+        "st_track_pick"."c_doc_no" as "c_doc_no",
+        "cust"."c_code" as "c_cust_code",
+        "cust"."c_name" as "c_cust_name",
+        2 as "n_completed",
+        "isnull"("route_mst"."c_name",'') as "c_route",
+        "route_mst"."c_code" as "c_route_code",
+        0 as "n_tray_state"
+        from "DBA"."st_track_pick" left outer join "st_track_complete_date" on "st_track_pick"."c_doc_no" = "st_track_complete_date"."c_doc_no"
+          and "st_track_pick"."c_tray_code" = "st_track_complete_date"."c_tray_code"
+          join "st_track_mst" on "st_track_pick"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_pick"."n_inout" = "st_track_mst"."n_inout"
+          join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
+          left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
+          left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
+        where "date"("isnull"("st_track_complete_date"."t_time","st_track_pick"."t_time")) = "DBA"."uf_default_date"()
+        and "isnull"("n_confirm_qty",0)+"isnull"("n_reject_qty",0) > 0;
     ------------------------------------------------------------------------
-    //        select "tray_move"."c_doc_no" as "c_doc_no",
-    //          "cust"."c_code" as "c_cust_code",
-    //          "cust"."c_name" as "c_cust_name",
-    //          (if "tray_move"."n_inout" = 9 then 1 else 0 endif) as "n_completed",
-    //          "tray_move"."c_tray_code" as "c_tray",
-    //          "route_mst"."c_code" as "c_route_code",
-    //          "isnull"("route_mst"."c_name",'') as "c_route_name",
-    //          "tray_move"."n_flag" as "n_tray_state"
-    //          from "st_track_tray_move" as "tray_move"
-    //            join "st_track_mst" on "tray_move"."c_doc_no" = "st_track_mst"."c_doc_no" --AND st_track_mst.n_complete <> 9
-    //            join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
-    //            left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
-    //            left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
-    //          where "isnull"("tray_move"."c_godown_code",'-') = '-'
-    //          and "tray_move"."n_inout" not in( 1,8 ) union
-    //        select "packing_done"."c_doc_no",
-    //          "packing_done"."c_cust_code",
-    //          "packing_done"."c_cust_name" as "c_cust_name",
-    //          "packing_done"."n_completed" as "n_completed",
-    //          "packing_done"."c_tray" as "c_tray",
-    //          "packing_done"."c_route_code" as "c_route_code",
-    //          "isnull"("packing_done"."c_route",'') as "c_route_name",
-    //          0 as "n_tray_state"
-    //          from "packing_done" union
-    //        select "uas_list"."c_doc_no",
-    //          "uas_list"."c_cust_code",
-    //          "uas_list"."c_cust_name" as "c_cust_name",
-    //          "uas_list"."n_completed" as "n_completed",
-    //          "uas_list"."c_tray" as "c_tray",
-    //          "uas_list"."c_route_code" as "c_route_code",
-    //          "isnull"("uas_list"."c_route",'') as "c_route_name",
-    //          0 as "n_tray_state"
-    //          from "uas_list"
-    //          order by "c_route_name" asc,"c_doc_no" asc for xml raw,elements
     select "tray_move"."c_doc_no" as "c_doc_no",
       "cust"."c_code" as "c_cust_code",
       "cust"."c_name" as "c_cust_name",
@@ -168,28 +126,25 @@ commit work;
         left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
       where "isnull"("tray_move"."c_godown_code",'-') = '-'
       and "tray_move"."n_inout" not in( 1,8 ) union
-    select distinct "st_track_pick"."c_doc_no" as "c_doc_no","cust"."c_code" as "c_cust_code","cust"."c_name" as "c_cust_name",2 as "n_completed",
-      "st_track_pick"."c_tray_code" as "c_tray","route_mst"."c_code" as "c_route_code","isnull"("route_mst"."c_name",'') as "c_route_name",0 as "n_tray_state"
-      from "DBA"."st_track_pick" left outer join "st_track_complete_date" on "st_track_pick"."c_doc_no" = "st_track_complete_date"."c_doc_no"
-        and "st_track_pick"."c_tray_code" = "st_track_complete_date"."c_tray_code"
-        join "st_track_mst" on "st_track_pick"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_pick"."n_inout" = "st_track_mst"."n_inout"
-        join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
-        left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
-        left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
-      where "date"("isnull"("st_track_complete_date"."t_time","st_track_pick"."t_time")) = "DBA"."uf_default_date"()
-      and "isnull"("n_confirm_qty",0)+"isnull"("n_reject_qty",0) > 0 union
-    select distinct "st_track_mst"."c_doc_no" as "c_doc_no","cust"."c_code" as "c_cust_code","cust"."c_name" as "c_cust_name",0 as "n_completed",
-      'UA-'+"isnull"("st_track_det"."c_stage_code",'NULL') as "c_tray","route_mst"."c_code" as "c_route_code",("route_mst"."c_name") as "c_route_name",0 as "n_tray_state"
-      from "st_track_mst"
-        join "DBA"."st_track_det" on "st_track_det"."c_doc_no" = "st_track_mst"."c_doc_no" and "st_track_det"."n_inout" = "st_track_mst"."n_inout"
-        join "act_mst" as "cust" on "cust"."c_code" = "isnull"("st_track_mst"."c_cust_code",'GC01')
-        left outer join "st_track_tray_move" as "tray_move" on "tray_move"."c_doc_no" = "st_track_mst"."c_doc_no"
-        left outer join "act_route" as "route" on "route"."c_br_code" = "st_track_mst"."c_cust_code" and "route"."c_code" = "st_track_mst"."c_br_code"
-        left outer join "route_mst" on "isnull"("route"."c_route_code",'-') = "route_mst"."c_code"
-      where "st_track_det"."c_tray_code" is null
-      and "isnull"("st_track_det"."c_godown_code",'-') = '-'
-      and "st_track_mst"."n_confirm" = 1
-      and "st_track_mst"."n_inout" not in( 1,8 ) for xml raw,elements
+    select "packing_done"."c_doc_no",
+      "packing_done"."c_cust_code",
+      "packing_done"."c_cust_name" as "c_cust_name",
+      "packing_done"."n_completed" as "n_completed",
+      "packing_done"."c_tray" as "c_tray",
+      "packing_done"."c_route_code" as "c_route_code",
+      "isnull"("packing_done"."c_route",'') as "c_route_name",
+      0 as "n_tray_state"
+      from "packing_done" union
+    select "uas_list"."c_doc_no",
+      "uas_list"."c_cust_code",
+      "uas_list"."c_cust_name" as "c_cust_name",
+      "uas_list"."n_completed" as "n_completed",
+      "uas_list"."c_tray" as "c_tray",
+      "uas_list"."c_route_code" as "c_route_code",
+      "isnull"("uas_list"."c_route",'') as "c_route_name",
+      0 as "n_tray_state"
+      from "uas_list"
+      order by "c_route_name" asc,"c_doc_no" asc for xml raw,elements
   --insert into Serv_log  (c_name,t_ltime) values('End',now());	  
   when 'get_current_tray_position' then
     select "tray_out"."c_tray_code",
@@ -214,4 +169,4 @@ commit work;
         on "carton_mst"."c_tray_code" = "tr"."c_tray_code" and "tr"."dt" = "carton_mst"."d_ldate"
       order by 1 asc for xml raw,elements
   end case
-end;
+end
